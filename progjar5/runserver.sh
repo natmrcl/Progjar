2e8c98f77ae432646c2559370726d0671f0e5e2b
natmrcl <natyamm10@gmail.com>
2023-07-03 14:22:19 +0700

    add files

---
+++ progjar5/runserver.sh
@@ -0,0 +1,17 @@
+#jalankan 5 async_server
+
+# python3 async_server.py 9000 &
+# python3 async_server.py 9001 &
+# python3 async_server.py 9002 &
+# python3 async_server.py 9003 &
+
+
+#jalankan 5 server_process_pool
+
+python3 server_process_pool_http.py 8000 &
+python3 server_process_pool_http.py 8001 &
+python3 server_process_pool_http.py 8002 &
+python3 server_process_pool_http.py 8004 &
+
+
+
